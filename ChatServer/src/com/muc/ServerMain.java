5f9c12d31ef7f43b6b0b6067103ce9069333dd02
Fabian5150 <fabian.strauch.04@web.de>
2021-09-12 13:55:56 +0200

    Simple Multithread Server

---
+++ ChatServer/src/com/muc/ServerMain.java
@@ -0,0 +1,44 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true){
+                System.out.println("About to accept client connection");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted Connection from " + clientSocket);
+                Thread t = new Thread(){
+                    @Override
+                    public void run(){
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for (int i = 0; i < 10; i++) {
+            outputStream.write((i + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
+    }
+}
